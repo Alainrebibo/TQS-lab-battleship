eee4deebbbdd0ef70b98123d5747753c31712d88
Juan David Ramírez Girón <33266718+jdmac94@users.noreply.github.com>
2020-10-25 19:09:41 +0100

    Project setup

---
+++ src/test/java/CoordinateTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+  @Test
+  void getX() {
+  }
+
+  @Test
+  void getY() {
+  }
+}
