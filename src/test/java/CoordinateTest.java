9d9aedf58e24a6a3fbd4dcd23ad5df6d7cbebf75
Juan David Ramírez Girón <33266718+jdmac94@users.noreply.github.com>
2020-11-11 16:29:20 +0100

    Run ok

--- src/test/java/CoordinateTest.java
+++ src/test/java/CoordinateTest.java
@@ -6,9 +6,19 @@ class CoordinateTest {
 
   @Test
   void getX() {
+
+    Coordinate coordinate = new Coordinate(1,2);
+    int x = coordinate.getX();
+
+    assertEquals(x, 1);
   }
 
   @Test
   void getY() {
+
+    Coordinate coordinate = new Coordinate(1,2);
+    int y = coordinate.getY();
+
+    assertEquals(y, 2);
   }
 }
