7b2324419644d84dcd32cb2b90024779d30894af
Juan David Ramírez Girón <33266718+jdmac94@users.noreply.github.com>
2020-11-11 17:50:57 +0100

    Correct test Player

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class PlayerTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+  @Test
+  void testPlayerName(){
+
+    String name = "Manolo";
+    Player player = new Player(name);
+    assertEquals(player.getName(), name);
+
+  }
+
+  @Test
+  void testPlayerHasWon(){
+
+    Player player = new Player(name);
+    player.setWon(true);
+    assertEquals(player.hasWon(), true);
+
+  }
+
+  @Test
+  void testPlayerStartsWithoutWinning(){
+
+    Player player = new Player(name);
+    assertEquals(player.hasWon(), false);
+
+  }
+
+  @Test
+  void testPlayerHasLose(){
+
+    Player player = new Player(name);
+    assertEquals(player.hasWon(), false);
+
+  }
+
+
+
 }
