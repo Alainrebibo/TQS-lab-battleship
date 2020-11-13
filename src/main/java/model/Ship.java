f019a2fa0104883ebcf196ac972a8d7f005c3584
SantiUAB <profSantiRP@gmail.com>
2020-11-13 21:54:41 +0100

    testear tamaño de ship

---
+++ src/main/java/model/Ship.java
@@ -0,0 +1,12 @@
+package model;
+
+
+public class Ship {
+    public Ship(Coordinate coo, int size, Direction horizontal) {
+
+    }
+
+    public int getSize() {
+        return -1;
+    }
+}
--- src/test/java/ShipTest.java
+++ src/test/java/ShipTest.java
@@ -1,12 +1,56 @@
 import model.Board;
+import model.Coordinate;
+import model.Direction;
+import model.Ship;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShipTest {
 
+  /*TODO crear un test para cada barco diferente:
+  * 4 size
+  * 3 size
+  * 2 size
+  * 1 size*/
+
+  Coordinate coor00;
+  Ship ship ;
+
+  @Before
+  public void setUp() throws  Exception{
+
+    coor00 = new Coordinate(0,0);
+    ship = new Ship(coor00,1, Direction.HORIZONTAL);
+  }
+
   @Test
-  void testShipCreation(){
+  void testShipCreationSize1(){
+
+    assertEquals(1, ship.getSize());
 
   }
+
+  void testShipCreationSize2(){
+
+    ship = new Ship(coor00,2, Direction.HORIZONTAL);
+    assertEquals(2, ship.getSize());
+
+  }
+
+  void testShipCreationSize3(){
+
+    ship = new Ship(coor00,3, Direction.HORIZONTAL);
+    assertEquals(3, ship.getSize());
+  }
+
+  void testShipCreationSize4(){
+
+    ship = new Ship(coor00,4, Direction.HORIZONTAL);
+    assertEquals(4, ship.getSize());
+  }
+
 
   @Test
   void testShipCoordinatesInitialization(){
