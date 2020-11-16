69aae27f18ef9c7bf3df55825231ee5b8563c040
SantiUAB <profSantiRP@gmail.com>
2020-11-16 22:16:55 +0100

    uml path coverage

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -32,49 +32,4 @@ class PlayerTest {
 
   }
 
-  @Test
-  void testPlayerStartsWithoutWinning(){
-
-    Player player = new Player();
-    assertFalse(player.hasWon());
-  }
-
-  @Test
-  void testPlayerHasLose(){
-
-    Player player = new Player();
-    assertEquals(player.hasWon(), false);
-
-  }
-
-  @Test
-  public void getBoardTest(){
-    Board board = game.getPlayer1().getBoard();
-    Game gameAux = new Game();
-
-    Coordinate[][] matrixBoard = board.getMatrix();
-    Coordinate[][] matrixAux = gameAux.getPlayer1().getBoard().getMatrix();
-
-    for (int i = 0; i < board.getMatrix().length; i++) {
-      for (int j = 0; j < board.getMatrix().length; j++) {
-        assertEquals(matrixBoard[i][j].getState(), matrixAux[i][j].getState());
-      }
-    }
-  }
-
-  @Test
-  public void getBoardEnemyTest(){
-
-    Game gameAux = new Game();
-
-    Coordinate[][] matrixBoard = game.getPlayer1().getBoardEnemy().getMatrix();
-    Coordinate[][] matrixAux = gameAux.getPlayer1().getBoardEnemy().getMatrix();
-
-    for (int i = 0; i < matrixBoard.length; i++) {
-      for (int j = 0; j < matrixAux.length; j++) {
-        assertEquals(matrixBoard[i][j].getState(), matrixAux[i][j].getState());
-      }
-    }
-  }
-
 }
